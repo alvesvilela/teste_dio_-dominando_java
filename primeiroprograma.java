3962a1a9f88c10863edf7eb9b97b876c51345ee1
Rinaldo <rinaldotrade@gmail.com>
2022-05-12 01:26:20 -0300

    first commit

---
+++ primeiroprograma.java
@@ -0,0 +1,7 @@
+public class primeiroprograma {
+        public static void main(String args[]){
+                int a = 2;
+                int b = 3;
+                System.out.println("Hello fuckin World!" + (a+b));
+        }
+}
